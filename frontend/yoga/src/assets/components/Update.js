eab21a86ba81723dbbcf9cb4dea92cc07234b199
Digambar <digambartu@gmail.com>
2022-12-11 16:13:40 +0530

    added props to update modal

--- frontend/yoga/src/assets/components/Update.js
+++ frontend/yoga/src/assets/components/Update.js
@@ -1,10 +1,23 @@
 import React from 'react'
 import { Modal, Button, Form, Row, Col } from 'react-bootstrap';
+import months from './Months';
 
 const Update = (props) => {
+    const { firstName, lastName, email, batch, paidMonth } = props.member
+    const [mem, setMem] = useState({
+        email: email,
+        batch: batch,
+    })
+    const [feeStatus, setFeeStatus] = useState(paidMonth == months[new Date().getMonth()])
+    const [monthPaid, setMonthPaid] = useState(paidMonth)
+
     const submit = () => {
         props.onHide();
         console.log("submitted")
+    }
+    const handlePayment = () => {
+        setFeeStatus(!feeStatus)
+        setMonthPaid(months[new Date().getMonth()])
     }
     return (
         <Modal
@@ -25,16 +38,8 @@ const Update = (props) => {
                             Name
                         </Form.Label>
                         <Col sm="8">
-                            {/* <Form.Control type="text" plaintext readOnly value={`$${client.bal}`} /> */}
+                            <Form.Control type="text" plaintext readOnly value={firstName + ' ' + lastName} />
                             <Form.Control type="text" />
-                        </Col>
-                    </Form.Group>
-                    <Form.Group as={Row} className="mb-3" controlId="formPlaintextEmail">
-                        <Form.Label column sm="4">
-                            Email
-                        </Form.Label>
-                        <Col sm="8">
-                            <Form.Control type="email" defaultValue="email@example.com" />
                         </Col>
                     </Form.Group>
 
@@ -43,16 +48,26 @@ const Update = (props) => {
                             Fees paid for
                         </Form.Label>
                         <Col sm="8">
-                            <Form.Control type='text' defaultValue="December" />
+                            <Form.Control type='text' plaintext readOnly value={monthPaid} />
                         </Col>
                     </Form.Group>
+
+                    <Form.Group as={Row} className="mb-3" controlId="formPlaintextEmail">
+                        <Form.Label column sm="4">
+                            Email
+                        </Form.Label>
+                        <Col sm="8">
+                            <Form.Control type="email" name="email" value={mem.email} onChange={handleChange} />
+                        </Col>
+                    </Form.Group>
+
 
                     <Form.Group as={Row} className="mb-3" controlId="formPlaintextPassword">
                         <Form.Label column sm="4">
                             Batch
                         </Form.Label>
                         <Col sm="8">
-                            <Form.Select>
+                            <Form.Select name="batch" value={mem.batch} onChange={handleChange}>
                                 <option>6-7AM</option>
                                 <option>7-8AM</option>
                                 <option>8-9AM</option>
@@ -63,7 +78,7 @@ const Update = (props) => {
                 </Form>
             </Modal.Body>
             <Modal.Footer>
-                <Button variant="success" onClick={submit}>Pay Fees</Button>
+                <Button variant="success" disabled={feeStatus} onClick={handlePayment}>{feeStatus ? "Fees Paid" : "Pay Fees"}</Button>
                 <Button onClick={submit}>Update</Button>
                 <Button variant="danger" onClick={props.onHide}>Close</Button>
             </Modal.Footer>
